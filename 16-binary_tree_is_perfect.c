1a06ebdf15643a7b055daaab9dd69cbd556bf3d0
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-08-25 17:59:22 -0700

    tasks 16, 100, 101, 103, 104

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,32 +1,43 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_b - This is a function that measures the
- * height of a binary tree.
- * @tree: is a pointer to the root node of the tree to measure the height.
+ * binary_tree_size - This is a function that measures
+ * the size of a binary tree.
+ * @tree:  is a pointer to the root node of the tree to measure the size.
  *
- * Return: hieght of the node or 0.
+ * Return: size of the tree
  */
-size_t binary_tree_height_b(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	int a = 0, b = 0, c = 0;
+	int a = 0;
 
 	if (!tree)
-		return (c);
-	c = 1;
-	if (tree->left)
-	{
-		a += binary_tree_height_b(tree->left);
-	}
-	if (tree->right)
-	{
-		b += binary_tree_height_b(tree->right);
-	}
-	if (a > b)
-		c += a;
-	else
-		c += b;
-	return (c);
+		return (0);
+	a++;
+	a += binary_tree_size(tree->left);
+	a += binary_tree_size(tree->right);
+	return (a);
+}
+
+/**
+ * binary_tree_nodes - This is a function that counts
+ * the nodes with at least 1 child in a binary tree.
+ * @tree:  is a pointer to the root node of the
+ * tree to count the number of leaves.
+ *
+ * Return: the number of leaves in the tree or 0.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int a = 0;
+
+	if (!tree)
+		return (0);
+	if ((tree->left) || (tree->right))
+		a = 1;
+	a += binary_tree_nodes(tree->left);
+	a += binary_tree_nodes(tree->right);
+	return (a);
 }
 
 /**
@@ -38,14 +49,17 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int node;
+	int sizee;
+
 	if (!tree)
 		return (0);
-	if (binary_tree_height_b(tree->left) == binary_tree_height_b(tree->right))
-	{
-		binary_tree_is_perfect(tree->left);
-		binary_tree_is_perfect(tree->right);
-	}
+
+	sizee = binary_tree_size(tree);
+	node = binary_tree_nodes(tree);
+	if ((sizee + 1) == (1 << node))
+		return (1);
 	else
 		return (0);
-	return (1);
+	return (0);
 }
