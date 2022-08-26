53179fb24e923b5a04c5b8bc7c53e331b0850ce4
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-08-26 15:29:57 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -54,6 +54,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (1);
 
 	sizee = binary_tree_size(tree);
 	node = binary_tree_nodes(tree);
